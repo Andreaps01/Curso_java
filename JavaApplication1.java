280c5371746269c999b0df572a192d8139daeae5
Andrea del Rosario <48729388+Andreaps01@users.noreply.github.com>
2019-04-01 16:31:26 +0200

    ejercicio

---
+++ JavaApplication1.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author PC02-LAB02
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+  //variable byte
+  byte b1=10;
+  byte b2=0xa;
+  System.out.println("variable b1:"+b1);
+  System.out.println("variable b2:"+b2);
+  
+  //variable short
+  short s1=2;
+  System.out.println("varianle s1:"+s1);
+  
+  //variable char
+  char ch1=65, ch2='A';
+  System.out.println("variable ch1:"+ch1);
+  System.out.println("variable ch2:"+ch2);
+  
+  //variables enteras
+  int decimal=100;
+  int octal=0144;
+  int hexa=0*64;
+  System.out.println("variable decimal:"+decimal);
+  System.out.println("variable octal:"+octal);
+  System.out.println("variable hexa:"+hexa);
+  
+  //variable long
+  long  long1=10,long2=20L;
+  System.out.println("variable long1:"+long1);
+  System.out.println("variable long2:"+long2);
+  //variable float
+  float f1=15,f2=22.3F;
+  System.out.println("variable f1:"+f1);
+  System.out.println("variable f2:"+f2);
+  //variable double
+  double d1=11.0,d2=30.15D;
+  System.out.println("variable d1:"+d1);
+  System.out.println("variable d2:"+d2);
+    }
+    
+}
---
+++ JavaApplication2.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication2;
+
+/**
+ *
+ * @author PC02-LAB02
+ */
+public class JavaApplication2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String nombre="Andrea";
+        String apellido="Palomino";
+        
+        System.out.println("CONCATENACIÓN:"+nombre+" "+apellido);
+        System.out.println("NUEVALINEA:\n"+nombre+" "+apellido);
+        System.out.println("TABULADOR:\t"+nombre+" "+apellido);
+        System.out.println("RETROCESO:\b\b"+nombre+" "+apellido);
+        System.out.println("RETORNO DE CARRO:\r"+nombre+" "+apellido);
+        System.out.println("COMILLA SIMPLE:\'"+nombre+" "+apellido+"\'");
+        System.out.println("COMILLA DOBLE:\""+nombre+" "+apellido+"\"");
+        
+        // TODO code application logic here
+    }
+    
+}
