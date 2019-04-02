6d4bfc03e7e231f6b5c468ebf8013d78b6408fba
Andrea del Rosario <48729388+Andreaps01@users.noreply.github.com>
2019-04-02 04:46:39 -1200

    operadores 1

--- JavaApplication2.java
+++ JavaApplication2.java
@@ -7,7 +7,7 @@ package javaapplication2;
 
 /**
  *
- * @author PC02-LAB02
+ * @author PC01-LAB02
  */
 public class JavaApplication2 {
 
@@ -15,18 +15,55 @@ public class JavaApplication2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        String nombre="Andrea";
-        String apellido="Palomino";
-        
-        System.out.println("CONCATENACIÓN:"+nombre+" "+apellido);
-        System.out.println("NUEVALINEA:\n"+nombre+" "+apellido);
-        System.out.println("TABULADOR:\t"+nombre+" "+apellido);
-        System.out.println("RETROCESO:\b\b"+nombre+" "+apellido);
-        System.out.println("RETORNO DE CARRO:\r"+nombre+" "+apellido);
-        System.out.println("COMILLA SIMPLE:\'"+nombre+" "+apellido+"\'");
-        System.out.println("COMILLA DOBLE:\""+nombre+" "+apellido+"\"");
-        
         // TODO code application logic here
+        System.out.println("Operadores Aritmeticos:");
+int a = 1 + 2;
+int b = a * 4;
+int c = b / 2;
+int d = c - a;
+int e = -d;
+System.out.println("a = " + a);
+System.out.println("b = " + b);
+System.out.println("c = " + c);
+System.out.println("d = " + d);
+System.out.println("e = " + e);
+System.out.println("\nOperador Módulo (residuo):");
+System.out.println("x mod 10 = " + a % 2);
+System.out.println("\nOperador Compuesto:");
+//a = a + 4
+a += 2;
+b -= 4;
+c *= a;
+System.out.println("a = " + a);
+System.out.println("b = " + b);
+System.out.println("c = " + c);
+System.out.println("\nOperador Incremento:");
+//int a = a + 1;
+//Puede reescribierse como
+a++;
+System.out.println("a = " + a);
+//Preincremento/decremento (se incrementa antes de asignar el valor)
+c = ++a;
+//Postincremento/decremento (se incrementa despues de asignar el valor)
+//La siguiente vez que se evalua b, es cuando se incrementa
+d = b++;
+System.out.println("b = " + b);
+System.out.println("c = " + c);
+System.out.println("d = " + d);
+System.out.println("\nOperador relacional:");
+//a es menor que b
+boolean res = a < b;
+System.out.println("res = " + res);
+System.out.println("\nOperador Ternario:");
+int min = (a < b) ? a : b;
+System.out.println("min = " + min);
+System.out.println("\nOperador de Asignación:");
+int i, j, k;
+//cadena de asignación
+i = j = k = 100; // valor de x, y, and z igual a 100
+System.out.println("i = " + i);
+System.out.println("j = " + j);
+System.out.println("k = " + k);
     }
     
 }
