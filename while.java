16c4b98c3285e373def23a7c1df2a1b68f6612f8
Andrea del Rosario <48729388+Andreaps01@users.noreply.github.com>
2019-04-16 19:17:42 +0200

    Add files via upload

---
+++ while.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication2;
+
+/**
+ *
+ * @author PC19-LAB02
+ */
+public class JavaApplication2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int n=1;
+        while(n<=10){
+           
+            System.out.println(n);
+               n=n+1;      
+        }
+                
+    }
+    
+}
