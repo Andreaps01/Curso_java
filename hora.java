d825d324d9ad24b9e8342d1639163ff39528da63
Andrea del Rosario <48729388+Andreaps01@users.noreply.github.com>
2019-04-23 11:02:45 -0500

    Add files via upload

---
+++ hora.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication2;
+
+/**
+ *
+ * @author PC02-LAB02
+ */
+public class JavaApplication2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int t=2;
+        int n=4;
+  for(int h=t;h<=n;h++){
+      for(int m=00;m<=59;m++){
+          System.out.println(h+":"+m);
+      }
+  }
+    }
+    
+}
