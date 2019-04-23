abfa63c01b2e3dbc4f2baba318ba273a8f14961b
Andrea del Rosario <48729388+Andreaps01@users.noreply.github.com>
2019-04-23 11:03:59 -0500

    Add files via upload

---
+++ ingreso de datos.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package factorial;
+import java.util.Scanner;
+/**
+ *
+ * @author PC02-LAB02
+ */
+public class Factorial {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int n;
+        Scanner sc= new Scanner(System.in);
+        n=sc.nextInt();
+        System.out.println("ingreso: " +n);
+        
+                
+      
+    }
+    
+}
