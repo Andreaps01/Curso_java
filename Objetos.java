a0b4f97d9b92c83a662536e16996241d7f2ab70f
Andrea del Rosario <48729388+Andreaps01@users.noreply.github.com>
2019-05-14 11:56:25 -0500

    Add files via upload

---
+++ Objetos.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package objetos;
+
+/**
+ *
+ * @author PC02-LAB02
+ */
+public class Objetos {
+
+     String nombre;
+    String apellidoPaterno;
+    String apellidoMaterno;
+    String carrera;
+    int dni;
+    double pension;
+    String sede;
+    int codigo;
+    public void desplejaronjeto(){
+        System.out.println("nombre: "+nombre);
+        System.out.println("Apellido Paterno: "+apellidoPaterno);
+        System.out.println("apellido Materno: "+apellidoMaterno);
+        System.out.println("Carrera: "+carrera);
+        System.out.println("dni: "+dni);
+        System.out.println("pension: "+pension);
+        System.out.println("sede: "+sede);
+        System.out.println("codigo: "+codigo);
+        // TODO code application logic here
+    }
+    
+}
---
+++ objetoprueba.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package objetos;
+
+/**
+ *
+ * @author PC02-LAB02
+ */
+public class objetoprueba {
+     public static void main(String args[]){
+        Objetos p1=new Objetos();
+       
+       
+        p1.nombre="juan";
+        p1.apellidoPaterno="Esperanza";
+        p1.apellidoMaterno="Lara";
+        p1.carrera="computacion";
+        p1.dni=73814148;
+        p1.pension=250.00;
+        p1.sede="independencia";
+        p1.codigo=48264;
+        p1.desplejaronjeto();        
+    }
+  
+}
+
