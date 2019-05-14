daf834b1f5c3a95f1b897cc706836cfeb29322c3
Andrea del Rosario <48729388+Andreaps01@users.noreply.github.com>
2019-05-14 11:25:29 -0500

    Add files via upload

---
+++ persona.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package personas;
+
+/**
+ *
+ * @author PC02-LAB02
+ */
+public class persona {
+    String nombre;
+    String apellidoPaterno;
+    String apellidoMaterno;
+    public void desplegarNombre(){
+        System.out.println("nombre: "+nombre);
+        System.out.println("Apellido Paterno: "+apellidoPaterno);
+        System.out.println("apellido Materno: "+apellidoMaterno);
+        
+    }
+    
+}
---
+++ personaPrueba.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package personas;
+
+/**
+ *
+ * @author PC02-LAB02
+ */
+public class personaPrueba {
+    public static void main(String args[]){
+        persona p1=new persona();
+       
+        
+        p1.nombre="juan";
+        p1.apellidoPaterno="Esperanza";
+        p1.apellidoMaterno="Lara";
+        p1.desplegarNombre();
+    }
+}
